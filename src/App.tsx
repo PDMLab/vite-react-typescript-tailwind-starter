74ed9155acb279e9038dc0db787837cd172d9d7d
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2023-02-20 21:12:41 +0100

    refactor: fix tailwind wording / add logos for vite + tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React, { ReactElement, useState } from 'react'
 import logo from './logo.svg'
+import viteLogo from './vite.svg'
+import tailwindLogo from './tailwind.svg'
 
 function App(): ReactElement {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="border border-gray-50 rounded-xl p-20 shadow-xl">
+    <div className="p-20 border shadow-xl border-gray-50 rounded-xl">
       <header>
         <div className="flex justify-center">
-          <img src={logo} className="h-32 w-32 animate-spin-slow" alt="logo" />
+          <img src={viteLogo} className="w-32 h-32" alt="vite logo" />
+          <img src={logo} className="w-32 h-32" alt="React logo" />
+          <img
+            src={tailwindLogo}
+            className="w-32 h-32"
+            alt="Tailwind CSS logo"
+          />
         </div>
-        <p className="text-2xl pb-3">Hello Vite + React + tailwindcss!</p>
+        <p className="pb-3 text-2xl">Hello Vite + React + Tailwind CSS!</p>
         <p>
           <button
-            className="bg-purple-400 pl-2 pr-2 pt-1 pb-1 rounded text-sm text-purple-100"
+            className="pt-1 pb-1 pl-2 pr-2 text-sm text-purple-100 bg-purple-400 rounded"
             onClick={() => setCount((count) => count + 1)}
           >
             count is: {count}
           </button>
         </p>
-        <p className="pb-3 pt-3">
+        <p className="pt-3 pb-3">
           Edit{' '}
           <code className="border border-1 pl-1 pr-1 pb-0.5 pt-0.5 rounded border-purple-400 font-mono text-sm bg-purple-100 text-purple-900">
             src/App.tsx
@@ -51,7 +59,7 @@ function App(): ReactElement {
             target="_blank"
             rel="noopener noreferrer"
           >
-            tailwindcss Docs
+            Tailwind CSS Docs
           </a>
         </p>
       </header>
